ab2251051ee2fb96fafef3e0629e8abd1bd9d730
keyush-parwal <keyushparwal1@gmail.com>
2023-11-02 11:43:04 +0530

    Removed gfg stats

--- Coding-Contest-Tracker/coding-contest-tracker-app/src/components/Stats.js
+++ Coding-Contest-Tracker/coding-contest-tracker-app/src/components/Stats.js
@@ -27,33 +27,11 @@ const StatsComponent = ({ user, leetCodeUsername }) => {
     return (
         <div className="bg-gray-100 p-4 rounded-lg shadow-md">
             <h2 className="text-2xl font-bold mb-4">Stats</h2>
-            <div className="bg-white p-4 rounded-lg shadow-md">
-                <h3 className="text-lg font-semibold mb-2">Total Problems Solved</h3>
-                <div className="flex items-center">
-                    <svg width="160" height="160" viewBox="0 0 160 160" style={{ transform: 'rotate(-90deg)' }}>
-                        <circle r="70" cx="80" cy="80" fill="transparent" stroke="#e0e0e0" strokeWidth="12px"></circle>
-                        <circle
-                            r="70"
-                            cx="80"
-                            cy="80"
-                            fill="transparent"
-                            stroke="#60e6a8"
-                            strokeLinecap="round"
-                            strokeWidth="12px"
-                            strokeDasharray={`${circumference}px`}
-                            strokeDashoffset={`${offset}px`}
-                        ></circle>
-                    </svg>
-                    <div className="ml-4">
-                        <p className="text-2xl font-bold text-gray-800">{totalSolved} / {totalQuestions}</p>
-                    </div>
-                </div>
-            </div>
             {/* Render the external URL below the progress bar */}
             <div className="mt-4">
                 <iframe
                     title="External Content"
-                    src="https://leetcard.jacoblin.cool/iwilltrickyou?ext=activity&theme=light"
+                    src={`https://leetcard.jacoblin.cool/${leetCodeUsername}?ext=activity&theme=light`}
                     className="w-full h-96"
                 />
             </div>
